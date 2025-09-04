a794663405abbf6442b2f94d138f8873c50c4795
windowwelder <welderules@duck.com>
2025-09-04 22:15:16 +0700

    fix: place 'await' inside async function as it supposed to

--- task_3/index.ts
+++ task_3/index.ts
@@ -49,13 +49,12 @@ const BALLONS: { [key: string]: BallonI } = {
 
 // Ваш код здесь
 
-const publicBallon = Object.values(BALLONS).filter( el => el.isPublic)
-
-const publicBallonId = publicBallon.map( el => el.id)
-
-const publicBallonAmounts = await Promise.all(publicBallonId.map( el => fetchBallonAmount(el)))
-
-const sumOfPublicBallons = publicBallonAmounts.reduce( (a,b) => a + b, 0 )
+async function getSumOfPublicBallons() {
+	const publicBallon = Object.values(BALLONS).filter( el => el.isPublic)
+	const publicBallonId = publicBallon.map( el => el.id)
+	const publicBallonAmounts = await Promise.all(publicBallonId.map( el => fetchBallonAmount(el)))
+	const sumOfPublicBallons = publicBallonAmounts.reduce( (a,b) => a + b, 0 )
+}
 
 /* fetchBallonAmount() */
 
